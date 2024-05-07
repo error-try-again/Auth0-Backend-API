dcfcca5e7e824b693598c2912c77c93a5f0f2dbe
Yane Karov <yane.karov@gmail.com>
2024-05-08 00:37:22 +1000

    Summary:
       - Improved userActionSchema to also capture regex for apple account ids

--- src/schema/update-account-schema.ts
+++ src/schema/update-account-schema.ts
@@ -4,6 +4,6 @@ export const userActionSchema = Joi.object({
   data: Joi.object({
     email: Joi.string().email()
   }).optional(),
-  // Alphanumerical, greater than 12, includes | -
-  userId: Joi.string().pattern(/^[\dA-Za-z|-]{12,}$/)
+  // Alphanumerical, greater than 12, includes | - .
+  userId: Joi.string().pattern(/^[\d.A-Za-z|-]{13,}$/).required()
 });
